0ac2703fbe98b5923eb6da2a49eb9f834f4dea5f
David Bachowski <dave@whitespace.ninja>
2018-08-05 18:22:45 -0400

    Adding ability to change viewport size

--- src/menu_specs.js
+++ src/menu_specs.js
@@ -1,9 +1,17 @@
-import {ACTION_BACK_TO_GAME, ACTION_POP_MENU, ACTION_PUSH_MENU} from "./menu_actions.js";
+import {ACTION_INCREASE_VIEWPORT_H, ACTION_INCREASE_VIEWPORT_W, ACTION_BACK_TO_GAME, ACTION_POP_MENU, ACTION_PUSH_MENU} from "./menu_actions.js";
 
 export const SETTINGS_MENU = {
     type: "OPTIONS",
-    summaryText: "Menu to configure your game experience. Nothing here yet :(",
+    summaryText: "Configure your game experience.",
     options: [
+        {
+            optionText: "Increase Viewport Height",
+            actionMap: [{key: 'ENTER', action: ACTION_INCREASE_VIEWPORT_H}]
+        },
+        {
+            optionText: "Increase Viewport Width",
+            actionMap: [{key: 'ENTER', action: ACTION_INCREASE_VIEWPORT_W}]
+        },
         {
             optionText: "Back",
             actionMap: [{key: 'ENTER', action: ACTION_POP_MENU}]
@@ -13,15 +21,15 @@ export const SETTINGS_MENU = {
 
 export const HELP_MENU = {
     type: 'OPTIONS',
-    summaryText: "Use Firefox to play if you aren't already!! Press 'h' to toggle the help menu while in-game.",
+    summaryText: "Welcome to AsciiWeb! Firefox is the best browser for this game. Press 'h' to pause the game and Spacebar to FIRE.",
     options: [
-        {
-            optionText: "Options",
-            actionMap: [{key: 'ENTER', action: ACTION_PUSH_MENU, menu: SETTINGS_MENU}]
-        },
         {
             optionText: "Resume",
             actionMap: [{key: 'ENTER', action: ACTION_BACK_TO_GAME}]
+        },
+        {
+            optionText: "Options",
+            actionMap: [{key: 'ENTER', action: ACTION_PUSH_MENU, menu: SETTINGS_MENU}]
         }
     ]
 }
